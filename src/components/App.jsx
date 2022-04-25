fbaac648c0e61682ce26a69e688e55946564646c
Jonibek <munirovjonibek@gmail.com>
2022-04-25 12:46:10 +0500

    finished get data using axios

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,37 @@
 import React from "react";
 import SearchBar from "./SearchBar";
-const App = () => {
-  return (
-    <>
-      <h1 style={{ textAlign: "center", marginTop: "1rem" }}>App component</h1>
-      <SearchBar />
-    </>
-  );
-};
+import axios from "axios";
+class App extends React.Component {
+  state = {
+    kalit: "",
+  };
+  getSearchData = (keyword) => {
+    this.setState({ kalit: keyword });
+  };
+  componentDidUpdate() {
+    this.getResult();
+  }
+  getResult = async () => {
+    const data = await axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: this.state.kalit,
+      },
+      headers: {
+        Authorization: "Client-ID xF1Pf3JI8CQqisc7F4_ZVd0qyiFDqrhqby0sB2j3loY",
+      },
+    });
+    console.log(data.data);
+  };
+  render() {
+    return (
+      <>
+        <h1 style={{ textAlign: "center", marginTop: "1rem" }}>
+          App component
+        </h1>
+        <SearchBar getData={this.getSearchData} />
+      </>
+    );
+  }
+}
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,23 +1,39 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { keyword: "" };
+  }
+  inputMalumotOl = (e) => {
+    this.setState({ keyword: e.target.value });
+  };
+  onFormSubmit = (e) => {
+    e.preventDefault();
+  };
   render() {
     return (
-      <>
-        <div
+      <div>
+        <form
           className="ui fluid action input"
           style={{ width: "70%", margin: "0 auto" }}
+          // onSubmit={(e) => this.onFormSubmit(e)}
+          onSubmit={(e) => {
+            this.props.getData(this.state.keyword);
+            return this.onFormSubmit(e);
+          }}
         >
           <input
             type="text"
-            placeholder="Search..."
+            placeholder="Search Image..."
             style={{ margin: "2rem 0" }}
+            onChange={this.inputMalumotOl}
           />
           <button className="ui button" style={{ margin: "2rem 0" }}>
             Search
           </button>
-        </div>
-      </>
+        </form>
+      </div>
     );
   }
 }
